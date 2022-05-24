79a57050e0d7d1acb0acf42aedccf370ea1bd399
jun447 <fa20-bse-054@cuilahore.edu.pk>
2022-05-24 04:03:36 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+queue<int> rev(queue<int> q);
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        queue<int> q;
+        int n, var;
+        cin>>n;
+        while(n--)
+        {
+            cin>>var;
+            q.push(var);
+        }
+        queue<int> a=rev(q);
+        while(!a.empty())
+        {
+            cout<<a.front()<<" ";
+            a.pop();
+        }
+        cout<<endl;
+    }
+}// } Driver Code Ends
+
+
+//function Template for C++
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+    stack<int> s;
+    while(!q.empty()){
+        int element=q.front();
+        q.pop();
+        s.push(element);
+    }
+    while(!s.empty()){
+        int elememt=s.top();
+        s.pop();
+        q.push(elememt);
+    }
+    return q;
+}
